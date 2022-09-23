1a70a14d97962fb76aa335e3af537d8812e6d5ea
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 16:06:58 +0200

    creado el input del número de filas/columnas

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -1,4 +1,10 @@
+import sys
+class Tabla:
+    def crear_tabla():
+        filas = int(input("Introduce el número de filas "))
+        columnas = int(input("Introduce el número de columnas "))
 
+Tabla.crear_tabla()
 
 
 
