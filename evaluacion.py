663e3f42951d8a22b954956594d18d3b012dcac4
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 14:48:59 +0200

    creada la clase matriz

--- evaluacion.py
+++ evaluacion.py
@@ -1,5 +1,9 @@
-def string_to_array(s):
-    if s == '':
-        return []
-    else:
-        return s.split()
+class Matriz:
+    def __init__(self):
+        self.matriz = []
+        for i in range(4):
+            fila = []
+            for j in range(3):
+                fila.append(int(input("Ingrese el elemento ({},{}) de la matriz: ".format(i,j))))
+            fila.append(sum(fila))
+            self.matriz.append(fila)
