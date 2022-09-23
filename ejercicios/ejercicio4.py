6006e4a2e7fad5d1780c7b979b1e8e617d29038b
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 16:09:42 +0200

    creada la tabla con los * en función del tamaño

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -1,8 +1,14 @@
-import sys
 class Tabla:
     def crear_tabla():
         filas = int(input("Introduce el número de filas "))
         columnas = int(input("Introduce el número de columnas "))
+        if filas < 1 or filas > 9 or columnas < 1 or columnas > 9:
+                print("Número no válido")
+        else:
+            for i in range(filas):
+                print("")
+                for j in range(columnas):
+                    print(" * ", end='')
 
 Tabla.crear_tabla()
 
