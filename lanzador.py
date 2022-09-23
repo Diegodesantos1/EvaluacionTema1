cb8c0c8bbbed656d12a0e91862c99e6caefbcb0c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 17:04:57 +0200

    cambios en los prints

--- lanzador.py
+++ lanzador.py
@@ -2,6 +2,7 @@ def lanzador():
     eleccion=input("\n\nElige el ejercicio que desea ejecutar: \n 1: Ejercicio Matriz \n 2: Ejercicio Texto \n 3: Ejercicio Listas \n 4: Ejercicio Tabla\n 5: Finalizar el programa\n")
     if eleccion=="1":
         from ejercicios.ejercicio1 import Matriz
+        Matriz.crear_matriz()
         lanzador()
     elif eleccion=="2":
         from ejercicios.ejercicio2 import Texto
@@ -13,6 +14,7 @@ def lanzador():
         lanzador()
     elif eleccion=="4":
         from ejercicios.ejercicio4 import Tabla
+        Tabla.crear_tabla()
         lanzador()
     elif eleccion=="5":
         exit()
