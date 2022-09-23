6ef05291b18e51997a521b8eb36f20702201de5e
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 16:38:43 +0200

    arreglado el código para que no utilice iteración

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -1,17 +1,14 @@
 class Matriz:
-    def __init__(self):
-        self.matriz = []
-        for i in range(4):
-            fila = []
-            for j in range(3):
-                fila.append(int(input("Ingrese el elemento ({},{}) de la matriz: ".format(i,j))))
-            fila.append(sum(fila))
-            self.matriz.append(fila)
-    def __str__(self):
-        return str(self.matriz)
-    def formato_matriz(self):
-        for i in range(4):
-            for j in range(3):
-                print(self.matriz[i][j], end="")
-            print(self.matriz[i][3])
+    def crear_matriz():
+        n1=int(input("Introduce el elemento (1,1) ")) ; n2=int(input("Introduce el elemento (1,2) "))
+        n3=int(input("Introduce el elemento (1,3) ")) ; n4=int(input("Introduce el elemento (2,1) "))
+        n5=int(input("Introduce el elemento (2,2) ")) ; n6=int(input("Introduce el elemento (2,3) "))
+        n7=int(input("Introduce el elemento (3,1) ")) ; n8=int(input("Introduce el elemento (3,2) "))
+        n9=int(input("Introduce el elemento (3,3) "))
+        n10= n1 + n2 + n3 ; n11 = n4 + n5 + n6 ; n12 = n7 + n8 + n9
+        fila1= [n1,n2,n3, n10] ; fila2= [n4, n5, n6, n11] ; fila3 = [n7,n8,n9, n11]
+        print(f"La matriz es {fila1}, {fila2}, {fila3}")
+
+Matriz.crear_matriz()
+
 
