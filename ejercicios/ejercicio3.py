f9db36f909db94f665c31e425dedf4ca9def0b51
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 15:05:16 +0200

    añadido al rango 1 para que llegue al 0

--- ejercicios/ejercicio3.py
+++ ejercicios/ejercicio3.py
@@ -1,7 +1,7 @@
 class Listas:
     def __init__(self):
         self.lista1 = list(range(11))
-        self.lista2 = list(range(-10, 0))
+        self.lista2 = list(range(-10, 1))
     def __str__(self):
         return str(self.lista1) + str(self.lista2)
     def formato_lista(self):
