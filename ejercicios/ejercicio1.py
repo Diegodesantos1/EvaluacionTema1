4f3feabd1fd81a17f7c9ba75b71d0b7788df2f26
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 17:23:36 +0200

    acabado el ejercicio 1 usando recursividad

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -1,13 +1,18 @@
 class Matriz:
+    n= 0
+    matriz = []
     def crear_matriz():
-        n1=int(input("Introduce el elemento (1,1) ")) ; n2=int(input("Introduce el elemento (1,2) "))
-        n3=int(input("Introduce el elemento (1,3) ")) ; n4=int(input("Introduce el elemento (2,1) "))
-        n5=int(input("Introduce el elemento (2,2) ")) ; n6=int(input("Introduce el elemento (2,3) "))
-        n7=int(input("Introduce el elemento (3,1) ")) ; n8=int(input("Introduce el elemento (3,2) "))
-        n9=int(input("Introduce el elemento (3,3) "))
-        n10= n1 + n2 + n3 ; n11 = n4 + n5 + n6 ; n12 = n7 + n8 + n9
-        fila1= [n1,n2,n3, n10] ; fila2= [n4, n5, n6, n11] ; fila3 = [n7,n8,n9, n11]
-        print(f"La matriz es:\n\n{fila1}\n{fila2}\n{fila3}")
+        Matriz.n += 1
+        if Matriz.n <= 4:
+            numero =int(input("Introduce el primer elemento de la fila de la matriz "))
+            numero2 =int(input("Introduce el segundo elemento de la fila de la matriz "))
+            numero3 =int(input("Introduce el tercer elemento de la fila de la matriz "))
+            numero4 = numero + numero2 + numero3
+            fila= [numero, numero2, numero3, numero4]
+            Matriz.matriz.append(fila)
+            Matriz.crear_matriz()
+        else:
+            print(f"La matriz es:\n\n{Matriz.matriz}")
 
 
 
