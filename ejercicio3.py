0010a9ec3e2e532f24ad311192fdf7701718bbcf
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 15:07:24 +0200

    puesto 22 para que llegue al 20

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,7 @@
 class Listas:
     def __init__(self):
         self.lista1 = list(range(11)) ; self.lista2 = list(range(-10, 1))
-        self.lista3 = list(range(0, 20, 2))
+        self.lista3 = list(range(0, 22, 2))
     def __str__(self):
         return str(self.lista1) + str(self.lista2)
     def formato_lista(self):
