5a818371129d5cee75159087fb53e701053c0ca0
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 15:55:32 +0200

    creada la clase tabla y la función crear la tabla

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -1,1 +1,16 @@
 class Tabla:
+    def crear_tabla():
+        print("Introduce las dimensiones de la tabla:")
+        filas = int(input("Filas: "))
+        columnas = int(input("Columnas: "))
+        tabla = []
+        for i in range(filas):
+            fila = []
+            for j in range(columnas):
+                fila.append(int(input("Ingrese el elemento ({},{}) de la tabla: ".format(i,j))))
+            tabla.append(fila)
+        return tabla
+
+
+
+
