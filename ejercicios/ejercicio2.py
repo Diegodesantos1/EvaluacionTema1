d3b120df38cc6fb7b7b57057808794197fefe72e
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 15:01:11 +0200

    añadido los prints de contar el texto

--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -3,5 +3,12 @@ class Texto:
         self.texto = input("Escriba el texto que quiera contar: ")
     def __str__(self):
         return self.texto
+    def longitud_texto(self):
+        if len(self.texto) >= 3 and len(self.texto) < 10:
+            print("\nLa longitud del texto es mayor o igual que 3 y menor que 10 \n TRUE")
+        else:
+            print("\nLa longitud del texto no es mayor o igual que 3 y menor que 10\n FALSE")
 texto1 = Texto()
 print("Texto: {}".format(texto1))
+texto1.longitud_texto()
+
