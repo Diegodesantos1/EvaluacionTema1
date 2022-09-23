55710fea6d8a9f4f6f67b3e9c41fae8c1331f9b8
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 17:34:53 +0200

    arreglado el ejercicio 2

--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -1,7 +1,8 @@
 class Texto:
     def crear_texto():
         texto = str(input("Escriba el texto que quiera contar: "))
-        if len(texto) >= 3 and len(texto) < 10:
+        numpalabras = len(texto.split())
+        if numpalabras >= 3 and numpalabras < 10:
             print("\nLa longitud del texto es mayor o igual que 3 y menor que 10 \nTRUE")
         else:
             print("\nLa longitud del texto no es mayor o igual que 3 y menor que 10\nFALSE")
