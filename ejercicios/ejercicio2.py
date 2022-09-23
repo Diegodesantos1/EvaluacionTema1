57e2e32906ca4132661480c73c21d05acd4026d0
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-23 16:47:06 +0200

    cambios en los ejercicios simplificándolos

--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -1,11 +1,9 @@
 class Texto:
-    def __init__(self):
-        self.texto = input("Escriba el texto que quiera contar: ")
-    def __str__(self):
-        return self.texto
-    def longitud_texto(self):
-        if len(self.texto) >= 3 and len(self.texto) < 10:
+    def crear_texto():
+        texto= str(input("Escriba el texto que quiera contar: "))
+        if len(texto) >= 3 and len(texto) < 10:
             print("\nLa longitud del texto es mayor o igual que 3 y menor que 10 \n TRUE")
         else:
             print("\nLa longitud del texto no es mayor o igual que 3 y menor que 10\n FALSE")
 
+Texto.crear_texto
